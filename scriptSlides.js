0124635add608d2378e103b007dc78222a83acbb
lera-m <buhg.odessa@gmail.com>
2016-10-08 20:27:48 +0300

    add 'if' condition

--- scriptSlides.js
+++ scriptSlides.js
@@ -1,12 +1,4 @@
-/*
-var allGalleries = document.querySelectorAll(".slider");
 
-var firstGallery = allGalleries[0].querySelector(".slider-inner").getElementsByClassName("img");
-var secondGallery = allGalleries[1].querySelector(".slider-inner").getElementsByClassName("img");
-//secondGallery[1].className = secondGallery[1].className + " visible"
-
-console.log(secondGallery[1]);
-*/
 
 function createSlider(div){
     var sliderInner = div.querySelector(".slider-inner");
@@ -19,7 +11,6 @@ function createSlider(div){
 
     var arrowRight = div.querySelector('.right');
     
-    console.log(arrowLeft);
     
     function moveLeft() {
         sliderInner.style.left = (currentImg * -600) + "px"; 
@@ -27,13 +18,20 @@ function createSlider(div){
     
     function rightClick(event){
         event.preventDefault();
-        currentImg++;  
+        currentImg++;
+        if (currentImg === images.length){
+            currentImg = 0;
+        }
+          
         moveLeft(); 
     }    
     
     function leftClick(event){
         event.preventDefault();
-        currentImg--;  
+        if (currentImg === 0){
+            currentImg = images.length;
+        }
+        currentImg--; 
         moveLeft();
 
     }    
@@ -49,59 +47,8 @@ var allDivs = document.body.querySelectorAll(".slider");
 var a = allDivs[0];
 var b = allDivs[1];
 
-createSlider(a)
-createSlider(b)
+createSlider(a);
+createSlider(b);
 
 
-
-
-
-
-
-
-
-
-
-
-/*
-var gallery = [
-           "img/1.jpg", 
-           "img/2.jpg", 
-           "img/3.jpg", 
-           "img/4.jpg",
-           "img/5.jpg", 
-           "img/6.jpg", 
-           "img/7.jpg", 
-           "img/8.jpg"
-           ];
-           
-var temp = 0;
-//var pic;
-
-
-var arrowLeft = function(elem){
-    var img = elem.nextElementSibling.firstElementChild;
-    
-    if (temp > 0){
-        img.src = gallery[temp - 1];
-        temp--;
-    } else if (temp === 0) {
-        
-        temp = gallery.length;
-        img.src = gallery[temp - 1];
-    }
-};
-
-var arrowRight = function(elem){
-    var img = elem.previousElementSibling.firstElementChild;
-    
-    if (temp < gallery.length - 1){
-        img.src = gallery[temp + 1];
-        temp++;
-    } else if (temp === gallery.length - 1){
-        temp = 0;
-        img.src = gallery[temp];
-    }
-};
-*/
 
